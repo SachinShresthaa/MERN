ae6dc615ebfef9cc94b294e570d8198a00e1d073
Sachin Shrestha <sachins.newa1@gmail.com>
2026-05-22 21:35:41 +0545

    String template

---
+++ JavaScript/string.js
@@ -0,0 +1,10 @@
+let she = "Sushma";
+let he = "Sachin";
+let both = `she ${she} he ${he}`;
+console.log(both);
+
+let price = 10;
+let VAT = 0.25;
+
+let total = `Total: ${(price * (1 + VAT)).toFixed(2)}`;
+console.log(total);
